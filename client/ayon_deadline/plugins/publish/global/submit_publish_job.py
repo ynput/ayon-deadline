10682566f97ecd3d621f5793ed93ca72cd92a199
Petr Kalis <petr.kalis@gmail.com>
2024-11-11 15:42:14 +0100

    Fix removing job_info
    Co-authored-by: Mustafa Jafar <mustafataherzaky@outlook.com>

--- client/ayon_deadline/plugins/publish/global/submit_publish_job.py
+++ client/ayon_deadline/plugins/publish/global/submit_publish_job.py
@@ -466,7 +466,7 @@ class ProcessSubmittedJobOnFarm(pyblish.api.InstancePlugin,
 
         # Inject deadline url to instances to query DL for job id for overrides
         for inst in instances:
-            inst["deadline"] = instance.data["deadline"]
+            inst["deadline"] = instance.data["deadline"].copy()
             inst["deadline"].pop("job_info")
 
         # publish job file
