575f9bd9d46300109b0be731dffc7751efbd88c6
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-12-13 16:59:46 +0100

    inits adding level sequences to mrq

---
+++ client/ayon_deadline/plugins/publish/unreal/collect_render_items_into_mrq.py
@@ -0,0 +1,22 @@
+import pyblish.api
+import unreal
+
+
+class CollectRenderItemsIntoMRQ(pyblish.api.InstancePlugin):
+    """Collect publishable render items into MRQ."""
+
+    order = pyblish.api.CollectorOrder + 0.1
+    label = "Collect Publish Item into MRQ"
+    hosts = ["unreal"]
+    families = ["render"]
+
+    def process(self, instance):
+        ctx = instance.context.data
+        self.mrq = ctx["mrq"]  # collector before ensures it's present
+
+        # i'm assuming the mrq is empty at this point
+        new_job = self.mrq.allocate_new_job()
+        new_job.map = unreal.SoftObjectPath(instance.data["master_level"])
+        new_job.sequence = unreal.SoftObjectPath(instance.data["sequence"])
+        # new_job.set_configuration(instance.context.data["render_presets"][0])
+        self.log.debug(f"{new_job = }")
