df5dd2ddd558e924562647373a89566c533a1dfd
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-12-13 17:29:05 +0100

    implements serialization of queue asset

--- client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
+++ client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
@@ -1,5 +1,6 @@
 from ayon_core.pipeline import publish
-import unreal
+from unreal import MoviePipelineEditorLibrary
+
 
 class ExtractMRQAsManifest(publish.Extractor):
     label = "Extract Media Render Queue as Manifest"
@@ -8,3 +9,13 @@ class ExtractMRQAsManifest(publish.Extractor):
 
     def process(self, instance):
         self.mrq = instance.data["mrq"]
+        _, manifest = (
+            MoviePipelineEditorLibrary.save_queue_to_manifest_file(self.mrq)
+        )
+        manifest_string = (
+            MoviePipelineEditorLibrary.convert_manifest_file_to_string(
+                manifest
+            )
+        )
+        instance.data["mrq_manifest"] = manifest_string
+        self.log.info(f"MRQ Manifest written to: {manifest}")
