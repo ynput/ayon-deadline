99d5121135afb80815f0e861adeddeab00745c3b
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-12-20 11:02:00 +0100

    implements copy manifest to publish

--- client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
+++ client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
@@ -1,4 +1,12 @@
+import shutil
+from copy import deepcopy
+from pathlib import Path
+
+from ayon_core.pipeline import Anatomy
 from ayon_core.pipeline import publish
+from ayon_core.lib import StringTemplate
+
+
 from unreal import MoviePipelineEditorLibrary
 
 
@@ -12,6 +20,7 @@ class ExtractMRQAsManifest(publish.Extractor):
         self.project_data = deepcopy(instance.data["projectEntity"])
 
         self.serialize_mrq(instance)
+        self.copy_manifest_to_publish(instance)
 
     def serialize_mrq(self, instance):
         # serialize mrq to file and string
@@ -28,3 +37,39 @@ class ExtractMRQAsManifest(publish.Extractor):
             manifest_string  # save manifest string for potential submission via string
         )
         self.manifest_to_publish = Path(manifest).resolve()  # save manifest file for potential submission via file
+
+    def copy_manifest_to_publish(self, instance):
+        # initialize template data
+        anatomy = Anatomy(self.project_data["name"])
+        template_data = self.anatomy_data
+        template_data["root"] = anatomy.roots
+
+        # get current product name and append manifest, set ext to .utxt
+        template_data["product"]["name"] += "Manifest"
+        template_data["ext"] = "utxt"
+
+        # format the publish path
+        project_templates = self.project_data["config"]["templates"]
+        template_data["version"] = (
+            f"v{template_data['version']:0{project_templates['common']['version_padding']}d}"
+        )
+        # how can i build a @token?
+        _dir_template = project_templates["publish"]["default"][
+            "directory"
+        ].replace("@version", "version")
+        _file_template = project_templates["publish"]["default"][
+            "file"
+        ].replace("@version", "version")
+        dir_template = StringTemplate(_dir_template)
+        file_template = StringTemplate(_file_template)
+        publish_dir = Path(dir_template.format_strict(template_data))
+        publish_file = Path(file_template.format_strict(template_data))
+        publish_manifest = publish_dir / publish_file
+        self.log.debug(f"{publish_manifest = }")
+
+        if not publish_dir.exists():
+            self.log.info(f"Creating publish directory: {publish_dir}")
+            publish_dir.mkdir(parents=True)
+        self.log.debug(f"{self.manifest_to_publish = }")
+        shutil.copyfile(self.manifest_to_publish, publish_manifest)
+        instance.data["publish_mrq"] = publish_manifest.as_posix()
