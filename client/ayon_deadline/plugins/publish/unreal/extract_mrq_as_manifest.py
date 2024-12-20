8d703ff6b43d577c652d1e754955bdeafc053ed2
Tony Dorfmeister <tony.dorfmeister@bepic.studio>
2024-12-20 11:01:43 +0100

    refactors serialization into function

--- client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
+++ client/ayon_deadline/plugins/publish/unreal/extract_mrq_as_manifest.py
@@ -8,14 +8,23 @@ class ExtractMRQAsManifest(publish.Extractor):
     families = ["render", "render.farm"]
 
     def process(self, instance):
+        self.anatomy_data = deepcopy(instance.data["anatomyData"])
+        self.project_data = deepcopy(instance.data["projectEntity"])
+
+        self.serialize_mrq(instance)
+
+    def serialize_mrq(self, instance):
+        # serialize mrq to file and string
         self.mrq = instance.data["mrq"]
-        _, manifest = MoviePipelineEditorLibrary.save_queue_to_manifest_file(
-            self.mrq
+        _, manifest = (
+            MoviePipelineEditorLibrary.save_queue_to_manifest_file(self.mrq)
         )
         manifest_string = (
             MoviePipelineEditorLibrary.convert_manifest_file_to_string(
                 manifest
             )
         )
-        instance.data["mrq_manifest"] = manifest_string
-        self.log.info(f"MRQ Manifest written to: {manifest}")
+        instance.data["mrq_manifest"] = (
+            manifest_string  # save manifest string for potential submission via string
+        )
+        self.manifest_to_publish = Path(manifest).resolve()  # save manifest file for potential submission via file
