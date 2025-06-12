229d6a00eb6fd85dc923fa4793846f985c34cab1
Kayla Man <moonyuet2008@hotmail.com>
2025-06-12 18:33:02 +0800

    edit remote publish.py

--- client/ayon_deadline/scripts/remote_publish.py
+++ client/ayon_deadline/scripts/remote_publish.py
@@ -20,6 +20,9 @@ def remote_publish(log):
         log.info(f"Setting active state {active} for instance: {instance}")
         if active:
             instance["active"] = active
+            instance["farm"] = False
+        else:
+            instance["active"] = False
 
         pyblish_context.data["create_context"] = create_context
 
