17730d3e058b512da696011209136ec4148dbafa
Roy Nieterau <roy_nieterau@hotmail.com>
2025-12-08 09:32:00 +0100

    Apply suggestion from @BigRoy

--- client/ayon_deadline/abstract_submit_deadline.py
+++ client/ayon_deadline/abstract_submit_deadline.py
@@ -124,7 +124,7 @@ class AbstractSubmitDeadline(
         if plugin_info_data:
             self.apply_additional_plugin_info(plugin_info_data)
 
-        # Some DCC -Houdini atm- have `render_target` in `creator_attributes`.
+        # Houdini has  `render_target` in `creator_attributes`.
         # When `render_target` is set `local_export_farm_render`, we want to
         # skip submitting the first job which is an export job.
         creator_attr = instance.data.get("creator_attributes", {})
