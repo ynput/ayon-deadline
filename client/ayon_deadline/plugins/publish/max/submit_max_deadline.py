d9a1edebcbc9802f839bba8e95f3569dcb74e4f9
Kayla Man <moonyuet2008@hotmail.com>
2026-08-14 14:37:10 +0800

    use os.path.normpath()

--- client/ayon_deadline/plugins/publish/max/submit_max_deadline.py
+++ client/ayon_deadline/plugins/publish/max/submit_max_deadline.py
@@ -436,7 +436,7 @@ class MaxSubmitDeadline(abstract_submit_deadline.AbstractSubmitDeadline,
         render_dir = Path(os.path.dirname(first_file))
         render_dir.mkdir(parents=True, exist_ok=True)
         script_path = render_dir / "pre_load_max_script.ms"
-        script_path = str(script_path.resolve())
+        script_path = os.path.normpath(script_path.as_posix())
 
         try:
             with open(script_path, "w") as script_file:
